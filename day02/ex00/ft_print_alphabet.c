1f9e45855373abbfbcdc4e684045225055b2acdc
Bonwoo Koo <bonwookoo@Bonwooui-MacBook-Pro.local>
2018-05-23 22:18:04 -0700

    : upload

---
+++ day02/ex00/ft_print_alphabet.c
@@ -0,0 +1,28 @@
+void my_putchar(char c)
+{
+   write(1, &c, 1);
+ }
+
+int main(void)
+{
+
+  int a = 96;
+    
+   
+  while (a < 128)
+    {
+      
+     
+      a++;
+    
+  int letter;
+  my_putchar(letter);
+  letter = a;}
+  
+  return (0);
+}
+
+void ft_print_alphabet(void);
+
+
+
---
+++ day02/ex02/ft_print_number.c
@@ -0,0 +1,29 @@
+void my_putchar(char c)
+{
+   write(1, &c, 1);
+ }
+
+int main(void)
+{
+
+  int a = 47;
+    
+   
+  while (a < 58)
+    {
+      
+     
+      a++;
+      int letter;
+      my_putchar(letter);
+      
+      letter = a; 
+    }
+  
+  return (0);
+}
+
+void ft_print_number (void);
+
+
+
